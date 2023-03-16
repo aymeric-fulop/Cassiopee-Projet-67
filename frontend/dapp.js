ecba66cdb64890972b7902440e750b2b62ebca45
Paul B <gamespaul@outlook.fr>
2023-03-16 01:34:47 +0100

    handled web3 connection in dapp

--- frontend/dapp.js
+++ frontend/dapp.js
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
 import Web3 from 'web3';
-import DepositContract from './contracts/DepositContract.json';
+import DepositContract from 'adrr';
 
-const web3 = new Web3(Web3.givenProvider);
+const web3 = new Web3();
 
 function App() {
   const [userAddress, setUserAddress] = useState(null);
+  const [amount, setAmount] = useState('');
+
+  const handleConnect = async () => {
+    if (window.ethereum) {
+      await window.ethereum.enable();
+      web3.setProvider(window.ethereum);
+    } else if (window.web3) {
+      web3.setProvider(window.web3.currentProvider);
+    } else {
+      console.error('No web3 provider found');
+    }
+  };
 
   const handleDeposit = async () => {
     const accounts = await web3.eth.getAccounts();
@@ -16,11 +28,10 @@ function App() {
 
     await depositContract.methods.deposit().send({
       from: accounts[0],
-      value: inputed_value
+      value: web3.utils.toWei(amount, 'ether')
     });
 
     setUserAddress(accounts[0]);
-
     const response = await fetch('/api/deposit', {
       method: 'POST',
       body: JSON.stringify({ userAddress: accounts[0] }),
@@ -35,7 +46,11 @@ function App() {
       {userAddress ? (
         <p>Deposit successful! Your address: {userAddress}</p>
       ) : (
-        <button onClick={handleDeposit}>Make deposit</button>
+        <>
+          <button onClick={handleConnect}>Connect to Web3</button>
+          <input type="text" value={amount} onChange={e => setAmount(e.target.value)} />
+          <button onClick={handleDeposit}>Make deposit</button>
+        </>
       )}
     </div>
   );
