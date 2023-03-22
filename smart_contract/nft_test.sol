74b8268979e0d3bd6dafac890fc2f0b07e987dc9
aymeric.fulop <aymeric.fulop@telecom-sudparis.eu>
2023-03-22 13:58:23 +0100

    Add of nft_test.sol

---
+++ smart_contract/nft_test.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/utils/Counters.sol";
+
+//The purpose of this smart contract is to implement basic NFT to test nft_transfer.sol on Hyperledger Fabric
+
+contract MyNFT is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    uint256 public constant MAX_NFTS = 100;
+
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    function mint(address recipient) public returns (uint256) {
+        require(_tokenIds.current() < MAX_NFTS, "Maximum number of NFTs reached");
+
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+
+        return newItemId;
+    }
+}
