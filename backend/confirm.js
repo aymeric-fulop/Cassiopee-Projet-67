16c6969b3dc628a1faf1f4efb1730c9904850b73
Bitan <gamespaul@outlook.fr>
2023-05-03 17:54:07 +0200

    confirm.js changes

--- backend/confirm.js
+++ backend/confirm.js
@@ -9,18 +9,31 @@ const provider = new ethers.providers.JsonRpcProvider(providerUrl);
 const walletMnemonic = ethers.Wallet.fromMnemonic(mnemonic);
 const wallet = walletMnemonic.connect(provider);
 
-const contractAddress = 'your_contract_address_when_deployed';
+const contractAddress = '0x9b4d86c7dbb039fb060b25096eadafc3e54e3944';
 const contract = new ethers.Contract(contractAddress, contractABI, wallet);
 
-const recipient = 'this_address_will_be_sent_by_the_dapp';
-const amount = ethers.utils.parseEther('1'); // Convert 1 ether to wei
+const depositor = '0xDA242f571090f25114A849F7fB3E7947D03D322e'; // Replace with the actual depositor address
+const recipient = '0xDA242f571090f25114A849F7fB3E7947D03D322e';
 
 async function confirmTransfer() {
-    const sender = wallet.address;
-    console.log(`Confirming transfer: sender=${sender}, recipient=${recipient}, amount=${amount}`);
-    const tx = await contract.confirm(recipient, amount);
+    const executor = wallet.address;
+    console.log(`Confirming transfer: executor=${executor}, depositor=${depositor}, recipient=${recipient}`);
+    
+    // Call the deposit function to simulate a deposit
+    const depositTx = await contract.deposit({ value: ethers.utils.parseEther('1') });
+    await depositTx.wait();
+    
+    // Get the balance of the depositor
+    const depositAmount = await contract.deposits(depositor);
+    
+    // Confirm the transfer
+    const tx = await contract.confirm(depositor, recipient);
     console.log(`Transaction hash: ${tx.hash}`);
     await tx.wait(); // Wait for the transaction confirmation
+    
+    // Check if the balance of the depositor is now 0
+    const newBalance = await contract.deposits(depositor);
+    console.log(`New balance of the depositor: ${newBalance}`);
 }
 
 confirmTransfer()
