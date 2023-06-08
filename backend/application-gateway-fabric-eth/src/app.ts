eb006bf2e1960d2af7cef412bd8bcdd94b07a9f3
Paul <gamespaul@outlook.fr>
2023-06-09 00:19:32 +0200

    Adjustements

--- backend/application-gateway-fabric-eth/src/app.ts
+++ backend/application-gateway-fabric-eth/src/app.ts
@@ -171,18 +171,6 @@ async function newSigner(): Promise<Signer> {
 }
 
 /**
- * This type of transaction would typically only be run once by an application the first time it was started after its
- * initial deployment. A new version of the chaincode deployed later would likely not need to run an "init" function.
- */
-async function initLedger(contract: Contract): Promise<void> {
-    console.log('\n--> Submit Transaction: InitLedger, function creates the initial set of assets on the ledger');
-
-    await contract.submitTransaction('InitLedger');
-
-    console.log('*** Transaction committed successfully');
-}
-
-/**
  * Evaluate a transaction to query ledger state.
  */
 async function getAllAssets(contract: Contract): Promise<void> {
@@ -195,77 +183,6 @@ async function getAllAssets(contract: Contract): Promise<void> {
     console.log('*** Result:', result);
 }
 
-/**
- * Submit a transaction synchronously, blocking until it has been committed to the ledger.
- */
-async function createAsset(contract: Contract): Promise<void> {
-    console.log('\n--> Submit Transaction: CreateAsset, creates new asset with ID, Color, Size, Owner and AppraisedValue arguments');
-
-    await contract.submitTransaction(
-        'CreateAsset',
-        'asset31',
-        'yellow',
-        '5',
-        'Tom',
-        '1300',
-    );
-
-    console.log('*** Transaction committed successfully');
-}
-
-/**
- * Submit transaction asynchronously, allowing the application to process the smart contract response (e.g. update a UI)
- * while waiting for the commit notification.
- */
-async function transferAssetAsync(contract: Contract, assetId : string): Promise<void> {
-    console.log('\n--> Async Submit Transaction: TransferAsset, updates existing asset owner');
-
-    const commit = await contract.submitAsync('TransferAsset', {
-        arguments: [assetId, 'Saptha'],
-    });
-    const oldOwner = utf8Decoder.decode(commit.getResult());
-
-    console.log(`*** Successfully submitted transaction to transfer ownership from ${oldOwner} to Saptha`);
-    console.log('*** Waiting for transaction commit');
-
-    const status = await commit.getStatus();
-    if (!status.successful) {
-        throw new Error(`Transaction ${status.transactionId} failed to commit with status code ${status.code}`);
-    }
-
-    console.log('*** Transaction committed successfully');
-}
-
-async function readAssetByID(contract: Contract, assetId : string): Promise<void> {
-    console.log('\n--> Evaluate Transaction: ReadAsset, function returns asset attributes');
-
-    const resultBytes = await contract.evaluateTransaction('ReadAsset', assetId);
-
-    const resultJson = utf8Decoder.decode(resultBytes);
-    const result = JSON.parse(resultJson);
-    console.log('*** Result:', result);
-}
-
-/**
- * submitTransaction() will throw an error containing details of any error responses from the smart contract.
- */
-async function updateNonExistentAsset(contract: Contract): Promise<void>{
-    console.log('\n--> Submit Transaction: UpdateAsset asset70, asset70 does not exist and should return an error');
-
-    try {
-        await contract.submitTransaction(
-            'UpdateAsset',
-            'asset70',
-            'blue',
-            '5',
-            'Tomoko',
-            '300',
-        );
-        console.log('******** FAILED to return an error');
-    } catch (error) {
-        console.log('*** Successfully caught the error: \n', error);
-    }
-}
 
 /**
  * envOrDefault() will return the value of an environment variable, or a default value if the variable is undefined.
@@ -287,23 +204,6 @@ async function displayInputParameters(): Promise<void> {
     console.log(`tlsCertPath:       ${tlsCertPath}`);
     console.log(`peerEndpoint:      ${peerEndpoint}`);
     console.log(`peerHostAlias:     ${peerHostAlias}`);
-}
-/**
- * get Ethereum address of a specific user
- **/
-async function getEthereumAddress(contract: Contract, userId : string): Promise<void> {
-    try{
-        const resultBytes = await contract.evaluateTransaction(
-        'GetEthereumAddress',
-        userId,
-    );
-        const resultJson = utf8Decoder.decode(resultBytes);
-        const result = JSON.parse(resultJson);
-        console.log(`Ethereum address of user ${userId} is ${result}`);
-    }
-    catch(err){
-        console.log(`Unable to get Ethereum address of user ${userId} !`);
-    }
 }
 
 /**
